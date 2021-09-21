01d6e36366b5a567bd27a628329ac8c13a47b5e5
Zahra <mirzakizahra@gmail.com>
2021-09-21 11:26:23 -0700

    Debug ValidDate Method

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         //for example:
         MyDate myDate = new MyDate(1399, 12, 30);
         System.out.println(myDate.nextDay().toString());
+        System.out.println(myDate.nextDay().toString());
+        System.out.println(myDate.valid_date(1399,11,30));
     }
 }
